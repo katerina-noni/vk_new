b2da36687d6998f0e22c66c0c2aedcbece84850f
katerina-noni <katya200990@mail.ru>
2019-11-30 15:55:59 +0300

    change project

--- VK App New/VK App New/Controller/UsersController.swift
+++ VK App New/VK App New/Controller/UsersController.swift
@@ -29,17 +29,16 @@ class UsersController: UITableViewController {
         Users(image: UIImage(named: "shkpj")!, name: "Петухов"),
         Users(image: UIImage(named: "shkpj")!, name: "Степанов")
     ]
-
-    var sortedUsers = [Character: [Users]]()
+    
     var filteredUsers = [Users]()
+    var sortedUsers = [Character: [Users]]()
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.setNavigationBarHidden(false, animated: true)
+        tableView.register(UINib(nibName: "UserXibCell", bundle: nil), forCellReuseIdentifier: "UserXibCell")
         
         self.sortedUsers = sort(user: user)
-        filteredUsers = user
     }
     
     private func sort(user: [Users]) -> [Character: [Users]] {
@@ -75,42 +74,53 @@ class UsersController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UsersCell", for: indexPath) as? UsersCell else {
-           preconditionFailure("UsersCell cannot be dequeued")
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserXibCell", for: indexPath) as? UserXibCell else {
+           preconditionFailure("UserXibCell cannot be dequeued")
         }
         
         let firstUser = sortedUsers.keys.sorted()[indexPath.section]
         let user = sortedUsers[firstUser]!
         let users: Users = user[indexPath.row]
-        cell.usersLabel.text = users.name
-        cell.usersImageView.image = users.image
+        cell.nameUserLabel.text = users.name
+        cell.fotoUserImageView.image = users.image
 
         return cell
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         if segue.identifier == "Show Detail Users",
-             let destinationVC = segue.destination as? UsersDetailController,
-             let indexPath = tableView.indexPathForSelectedRow {
-             let usersname = user[indexPath.row].name
-             destinationVC.title = usersname
-         }
-     }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//         if segue.identifier == "UserDetailPhotos",
+//             let destinationVC = segue.destination as? UsersDetailController,
+//             let indexPath = tableView.indexPathForSelectedRow {
+//             let usersname = user[indexPath.row].name
+//             destinationVC.title = usersname
+//         }
+//     }
     
-    @IBAction func logoutButton(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let destinationVC = UIStoryboard(
+            name: "Main",
+            bundle: nil).instantiateViewController(withIdentifier: "UserPhotos") as! UsersDetailController
+        
+        let keysection = sortedUsers.keys.sorted()[indexPath.section]
+        let detailInfo = sortedUsers[keysection]![indexPath.row]
+        
+        destinationVC.title = detailInfo.name
+        
+        navigationController?.pushViewController(destinationVC, animated: true)
     }
     
+    @IBAction func logoutUserButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
 
 extension UsersController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.isEmpty {
-            sortedUsers = sort(user: user)
-        } else {
             filteredUsers = user
-                .filter { $0.name.contains(searchText) }
-                .sorted { $0.name < $1.name }
+        } else {
+            filteredUsers = user.filter { $0.name.lowercased().contains(searchText.lowercased()) }
         }
+        sortedUsers = sort(user: filteredUsers)
         tableView.reloadData()
     }
 }
---
+++ VK App New/VK App New/View/UserXibCell.swift
@@ -0,0 +1,51 @@
+//
+//  UserXibCell.swift
+//  VK App New
+//
+//  Created by Екатерина on 30/11/2019.
+//  Copyright © 2019 noni. All rights reserved.
+//
+
+import UIKit
+
+class UserXibCell: UITableViewCell {
+    
+    @IBOutlet weak var nameUserLabel: UILabel!
+    
+    @IBOutlet weak var fotoUserImageView: UIImageView!
+    
+    @IBOutlet weak var frameUserView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        fotoUserImageView.layer.cornerRadius = frame.height / 2
+        fotoUserImageView.clipsToBounds = true
+        
+        fotoUserImageView.layer.shadowColor = UIColor.black.cgColor
+        fotoUserImageView.layer.backgroundColor = UIColor.clear.cgColor
+        fotoUserImageView.layer.shadowOffset = .init(width: 3, height: 2)
+        fotoUserImageView.layer.shadowRadius = 2
+        fotoUserImageView.layer.shadowOpacity = 0.3
+        fotoUserImageView.layer.shadowPath = UIBezierPath.init(roundedRect: frameUserView.bounds, cornerRadius: 150).cgPath
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        UIView.animate(withDuration: 0.6,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 0,
+                       options: [],
+                       animations: {
+                        self.fotoUserImageView.bounds.size.height *= 2
+        })
+    }
+    
+}
