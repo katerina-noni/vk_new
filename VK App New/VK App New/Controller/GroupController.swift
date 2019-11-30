9deae977b683da8f3b9718a28c8a479073a4f957
katerina-noni <katya200990@mail.ru>
2019-11-30 19:38:30 +0300

    change group controller

--- VK App New/VK App New/Controller/GroupController.swift
+++ VK App New/VK App New/Controller/GroupController.swift
@@ -10,40 +10,72 @@ import UIKit
 
 class GroupController: UITableViewController {
     
+    @IBOutlet weak var groupSearchBar: UISearchBar! {
+        didSet {
+            groupSearchBar.delegate = self
+        }
+    }
     var groups = [
-        Group(image: UIImage(named: "garage")!, name: "Group 1"),
-        Group(image: UIImage(named: "11")!, name: "Group 2"),
-        Group(image: UIImage(named: "1089093")!, name: "Group 3"),
-        Group(image: UIImage(named: "icon")!, name: "Group 4")
+        Group(image: UIImage(named: "garage")!, name: "One"),
+        Group(image: UIImage(named: "11")!, name: "Two"),
+        Group(image: UIImage(named: "1089093")!, name: "Three"),
+        Group(image: UIImage(named: "icon")!, name: "Four")
     ]
     
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    var filterGroup = [Group]()
+    var sortedGroup: [Character: [Group]] = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UINib(nibName: "GroupXibCell", bundle: nil), forCellReuseIdentifier: "GroupXibCell")
+              
+              self.sortedGroup = sortedGroupArray(array: groups)
+    }
+    
+    private func sortedGroupArray(array: [Group]) -> [Character:[Group]] {
+        var sortDict = [Character: [Group]]()
         
-        navigationController?.setNavigationBarHidden(false, animated: true)
-
+        array.forEach { group in
+            guard let firstChar = group.name.first else { return }
+            
+            if var sortedArray = sortDict[firstChar] {
+                sortedArray.append(group)
+                sortDict[firstChar] = sortedArray.sorted {
+                    $0.name < $1.name }
+            } else {
+                sortDict[firstChar] = [group]
+            }
+        }
+        return sortDict
     }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sortedGroup.keys.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let keySorted = sortedGroup.keys.sorted()[section]
+        return String(keySorted)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groups.count
+        let keySorted = sortedGroup.keys.sorted()
+        return sortedGroup[keySorted[section]]?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as? GroupCell else {
-           preconditionFailure("GroupCell cannot be dequeued")
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GroupXibCell", for: indexPath) as? GroupXibCell else {
+           preconditionFailure("GroupXibCell cannot be dequeued")
         }
         
-        print("Section: " + String(indexPath.section) + ", row: " + String(indexPath.row))
-        let groupname = groups[indexPath.row].name
-        let groupImage = groups[indexPath.row].image
-        cell.groupNameLabel.text = groupname
-        cell.groupImageView.image = groupImage
+        let firstChar = sortedGroup.keys.sorted()[indexPath.section]
+        let groupFirst = sortedGroup[firstChar]!
+        let group: Group = groupFirst[indexPath.row]
+        
+        cell.nameGroupLabel.text = group.name
+        cell.fotoImageView.image = group.image
+        
         return cell
     }
     @IBAction func addSelectedGroup(segue: UIStoryboardSegue) {
@@ -52,6 +84,7 @@ class GroupController: UITableViewController {
             let group = sourceVC.groups[indexPath.row]
             if !groups.contains(where:{ $0.name == group.name}) {
                 groups.append(group)
+                sortedGroup = sortedGroupArray(array: groups)
                 tableView.reloadData()
             }
         }
@@ -59,13 +92,51 @@ class GroupController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            groups.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            let firtstChar = sortedGroup.keys.sorted()[indexPath.section]
+            let groupFirst = sortedGroup[firtstChar]!
+            let group = groupFirst[indexPath.row]
+            
+            let initialSectionsCount = sortedGroup.keys.count
+            
+            groups.removeAll { $0.name == group.name }
+            
+            if (groupSearchBar.text ?? "").isEmpty {
+                filterGroup = groups
+            } else {
+                filterGroup = groups.filter {
+                    $0.name.lowercased().contains(groupSearchBar.text!.lowercased())
+                }
+            }
+            
+            sortedGroup = sortedGroupArray(array: filterGroup)
+            
+            if initialSectionsCount - sortedGroup.keys.count == 0 {
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            } else {
+                tableView.deleteSections(IndexSet([indexPath.section]), with: .automatic)
+            }
         }
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     @IBAction func logoutButtonGroup(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+}
+
+extension GroupController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filterGroup = groups
+        } else {
+            filterGroup = groups.filter{
+                $0.name.lowercased().contains(searchText.lowercased())
+            }
+        }
+        sortedGroup = sortedGroupArray(array: filterGroup)
+        tableView.reloadData()
+    }
 }
---
+++ VK App New/VK App New/View/GroupXibCell.swift
@@ -0,0 +1,36 @@
+//
+//  GroupXibCell.swift
+//  VK App New
+//
+//  Created by Екатерина on 30/11/2019.
+//  Copyright © 2019 noni. All rights reserved.
+//
+
+import UIKit
+
+class GroupXibCell: UITableViewCell {
+    
+    @IBOutlet weak var nameGroupLabel: UILabel!
+    
+    @IBOutlet weak var fotoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func layoutSubviews() {
+        fotoImageView.layer.cornerRadius = frame.height / 2
+        fotoImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        UIView.animateKeyframes(withDuration: 0.6,
+                                delay: 0,
+                                options: [],
+                                animations: {
+                                    self.fotoImageView.bounds.size.height *= 2
+        })
+    }
+    
+}
